262e12f692ffca306da87e8c4dcddc929c02b486
Hoang Huy <lehuyprox@gmail.com>
2020-06-21 12:50:16 +0700

    Create Account - Smart Contracts

--- server/services/smart_contract.js
+++ server/services/smart_contract.js
@@ -6,6 +6,7 @@ const web3 = new Web3("http://localhost:8545");
 const bytecode = fs.readFileSync('contracts/contracts_voting_sol_Voting.bin').toString()
 const abi = JSON.parse(fs.readFileSync('contracts/contracts_voting_sol_Voting.abi').toString())
 const deployedContract = new web3.eth.Contract(abi)
+var smartContract = {};
 
 smartContract.createAccount = () => {
     acc = web3.eth.accounts.create();
